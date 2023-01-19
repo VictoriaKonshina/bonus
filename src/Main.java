0e9bf0cbd452c990a192c98688be1f1a1f1ceffe
VictoriaKonshina <105608687+VictoriaKonshina@users.noreply.github.com>
2023-01-19 12:18:23 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,17 @@ public class Main {
 
         int balance = 100; //cумма счета
         int addBalance = 1100; //cумма пополнения
-        int sum = balance+addBalance; //итоговая сумма
+        int sum = balance + addBalance; //итоговая сумма
         int addBonus;
 
-        if (sum >=1000) {
+        if (sum >= 1000) {
             addBonus = addBalance / 100;
         } else {
             addBonus = 0;
         }
         int score = sum + addBonus;
 
-            System.out.println("Итоговый баланс счета:" + score);
-            System.out.println("Количество бонусов:" + addBonus);
-        }
+        System.out.println("Итоговый баланс счета:" + score);
+        System.out.println("Количество бонусов:" + addBonus);
     }
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
+}
